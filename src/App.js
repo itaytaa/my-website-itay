1d9e8499e4881f425e6b036b1fac643be78c352a
itay taasa <itaytaasa@gmail.com>
2021-04-01 13:31:55 +0300

    added favicon and title

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 
 import './App.css';
-
+import React, { useEffect } from 'react'
 import Main from './Main/Main';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Navbar from './Navbar/Navbar';
 import Contact from './Contact/Contact';
 import Projects from './Projects/Projects';
 
+
+
 function App() {
+
+  useEffect(() => {
+    document.title = "Itay Taasa"
+  }, []);
+
   return (
     <Router>
       <div className="App">
