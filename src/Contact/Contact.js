4ce848c83e3e4c858c002a6b53cd1df848a8bb6e
itay taasa <itaytaasa@gmail.com>
2021-04-01 13:01:25 +0300

    contact added

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -3,11 +3,12 @@ import './Contact.css'
 function Contact() {
     return (
         <div className="Contact">
-            <div>
-
-
+            <div className="contact-box">
+                <div><b>Tel:</b> +952-3919039</div>
+                <div><b>Email:</b>  Itaytaasa@gmail.com</div>
+                <div><b>GitHub:</b> https://github.com/itaytaa</div>
             </div>
-         
+
             <div className="clouds">
 
                 <div className="cloud x1"></div>
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -13,13 +13,13 @@ function Main(props) {
                 onInit={(typewriter) => {
                     typewriter.typeString("Hi, ").pauseFor(2000).start()
                     typewriter.typeString("My name is Itay.<br>").pauseFor(2000).start()
-                    typewriter.typeString("I'm a Web Developer.<br>").pauseFor(2000).start()
+                    typewriter.typeString("I'm a Full-stack Developer.<br>").pauseFor(2000).start()
                     typewriter.typeString("I've built this website using React.<br>").pauseFor(2000).start()
                     typewriter.typeString("I can build yours too...<br>").pauseFor(1500).start()
                     typewriter.typeString("if you pay me").pauseFor(1500).deleteChars(6).start()
                     typewriter.typeString("want :)<br>").pauseFor(2000).start()
                     typewriter.typeString("Lets talk!").pauseFor(1000).start()
-                    //  <br>   umm  <br>I\'ve add my projects that I built and it\'s all in the Gallery Tab .<br> click it and see what i\'ve been doing on friday nights in the last six months.  "
+
                 }}
 
             />
